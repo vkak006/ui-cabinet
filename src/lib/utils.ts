6f087d73803732927b2ed77a70cf16a6f8bd3f55
vkak006 <vkak006@datasolution.kr>
2024-11-22 17:33:45 +0900

    Init: variant 관련 dependencies 추가

---
+++ src/lib/utils.ts
@@ -0,0 +1,12 @@
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+// eslint-disable-next-line import/prefer-default-export
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function getLastSegment(path: string): string {
+  const segments = path.split('/');
+  return segments[segments.length - 1] || '';
+}
